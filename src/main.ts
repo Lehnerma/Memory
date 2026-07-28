a138f2ee65c9beea9722715ba06f63928fb8ccaf
Max Lehner <150380803+Lehnerma@users.noreply.github.com>
2026-07-28 02:55:26 +0200

    feat: add card component and game logic with styling

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,13 @@
 import "./styles/style.scss";
+init();
+
+function init(){
+    const fieldRef = document.getElementById("field");
+    if (!fieldRef) return;
+    fieldRef.addEventListener('click', e => {
+        const card = (e.target as HTMLElement).closest(".card") as HTMLButtonElement;
+        if (!card) return;
+        card.classList.toggle("flipped")
+        
+    })
+}
---
+++ vite.config.ts
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: ""  //hier können wir eine pfand angeben wie auf die sachen zugegriffen wird wenn das project nicht als root abgespeichert wird. 
+});
